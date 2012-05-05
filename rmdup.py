3ce667d6e9e9e49ff9cae2de48629e83558a0af5
Krisztian Fekete <fkr972@gmail.com>
2012-05-05 14:53:15 +0200

    is_duplicate

--- rmdup.py
+++ rmdup.py
@@ -126,18 +126,18 @@ class Test_TempDir(unittest.TestCase):
                 self.assertEquals(content, f.read())
 
 
-def same_files(fname1, fname2):
+def same_file(fname1, fname2):
     try:
         return os.stat(fname1) == os.stat(fname2)
     except OSError:
         return False
 
 
-class Test_same_files(unittest.TestCase):
+class Test_same_file(unittest.TestCase):
 
     def test_same_file(self):
         fname = EXISTING_FILE
-        self.assertTrue(same_files(fname, fname))
+        self.assertTrue(same_file(fname, fname))
 
     def test_different_files_same_content_is_not_same(self):
         with TempDir() as d:
@@ -150,13 +150,13 @@ class Test_same_files(unittest.TestCase):
             self.assertTrue(file_exists(f1))
             self.assertTrue(file_exists(f2))
 
-            self.assertFalse(same_files(f1, f2))
+            self.assertFalse(same_file(f1, f2))
 
     def  test_non_existing_files_are_not_same(self):
         # that is - no exceptions are raised!
         with TempDir() as d:
             f = d.subpath('non_existing_file')
-            self.assertFalse(same_files(f, f))
+            self.assertFalse(same_file(f, f))
 
 
 def _read_block(file):
@@ -215,8 +215,30 @@ class Test_same_content(unittest.TestCase):
         self.assertFalse(same_content(NON_EXISTING_FILE, NON_EXISTING_FILE))
 
 
+def is_duplicate(fname1, fname2):
+    return (not same_file(fname1, fname2)) and same_content(fname1, fname2)
+
+
+class Test_is_duplicate(unittest.TestCase):
+
+    def test_two_files_same_content_is_duplicate(self):
+        with TempDir() as d:
+            d.make_file('1', '')
+            d.make_file('2', '')
+            self.assertTrue(is_duplicate(d.subpath('1'), d.subpath('2')))
+
+    def test_different_content_not_duplicate(self):
+        with TempDir() as d:
+            d.make_file('1', '')
+            d.make_file('2', '2')
+            self.assertFalse(is_duplicate(d.subpath('1'), d.subpath('2')))
+
+    def test_same_file_is_not_duplicate(self):
+        self.assertFalse(is_duplicate(EXISTING_FILE, EXISTING_FILE))
+
+
 # is_duplicate(f1, f2)
-#     (not same_files) and same_content
+#     (not same_file) and same_content
 # treat_as_duplicate(orig_dir, duplicate_candidate, ignored_differences)
 #     files(duplicate_candidate) - files(orig_dir) - ignored_differences = set()
 #     (file2 in ignored_differences) or is_duplicate(file1, file2) for all common files in orig_dir, duplicate_candidate
@@ -225,61 +247,61 @@ class Test_same_content(unittest.TestCase):
 # remove_empty_subdirs(path)
 
 
-def filesize(fname):
-    if os.path.islink(fname):
-        return None
-    return os.path.getsize(fname)
+# def filesize(fname):
+#     if os.path.islink(fname):
+#         return None
+#     return os.path.getsize(fname)
 
 
-def files_in(directory):
-    '''directory -> [(fname, size)]
+# def files_in(directory):
+#     '''directory -> [(fname, size)]
 
-    Where files are relative to directory
-    '''
-    result = []
-    for d, subdirs, files in os.walk(directory):
-        reld = os.path.relpath(d, directory)
-        for f in files:
-            fname = os.path.join(reld, f)
-            fsize = filesize(os.path.join(directory, fname))
-            result.append((fname, fsize))
-    return sorted(result)
-
-
-def is_duplicate(dir1, dir2):
-    files1 = files_in(dir1)
-    files2 = files_in(dir2)
-    if files1 == files2:
-        return True
-
-    # give a difference:
-    f1 = set(files1)
-    f2 = set(files2)
-
-    only_f1 = f1 - f2
-    if only_f1:
-        print 'only in original ({0})'.format(orig)
-        pprint(sorted(only_f1))
-
-    only_f2 = f2 - f1
-    if only_f2:
-        print 'only in duplicate ({0})'.format(dup)
-        pprint(sorted(only_f2))
-
-    return False
+#     Where files are relative to directory
+#     '''
+#     result = []
+#     for d, subdirs, files in os.walk(directory):
+#         reld = os.path.relpath(d, directory)
+#         for f in files:
+#             fname = os.path.join(reld, f)
+#             fsize = filesize(os.path.join(directory, fname))
+#             result.append((fname, fsize))
+#     return sorted(result)
 
 
-def remove_dup(orig, duplicate):
-    if not os.path.exists(duplicate):
-        return
-    if os.path.abspath(orig) == os.path.abspath(duplicate):
-        raise NotDuplicate(orig, duplicate)
-    if not is_duplicate(orig, duplicate):
-        raise NotDuplicate(orig, duplicate)
+# def is_duplicate(dir1, dir2):
+#     files1 = files_in(dir1)
+#     files2 = files_in(dir2)
+#     if files1 == files2:
+#         return True
 
-    print 'remove duplicate {0}'.format(duplicate)
-    shutil.rmtree(duplicate)
-    print 'done'
+#     # give a difference:
+#     f1 = set(files1)
+#     f2 = set(files2)
+
+#     only_f1 = f1 - f2
+#     if only_f1:
+#         print 'only in original ({0})'.format(orig)
+#         pprint(sorted(only_f1))
+
+#     only_f2 = f2 - f1
+#     if only_f2:
+#         print 'only in duplicate ({0})'.format(dup)
+#         pprint(sorted(only_f2))
+
+#     return False
+
+
+# def remove_dup(orig, duplicate):
+#     if not os.path.exists(duplicate):
+#         return
+#     if os.path.abspath(orig) == os.path.abspath(duplicate):
+#         raise NotDuplicate(orig, duplicate)
+#     if not is_duplicate(orig, duplicate):
+#         raise NotDuplicate(orig, duplicate)
+
+#     print 'remove duplicate {0}'.format(duplicate)
+#     shutil.rmtree(duplicate)
+#     print 'done'
 
 
 orig_duplicate = [
